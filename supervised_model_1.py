77ca37d9d4ae01ef4eecddd43dcf1e79a95472eb
Kelvin <projectwave00@gmail.com>
2025-12-28 17:17:26 +0400

    Add files via upload

---
+++ supervised_model_1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Loading dataset
+df = pd.read_csv("student_habits_performance.csv")
+
+# Selecting numeric features
+
+X = df[
+    [
+        'age',
+        'study_hours_per_day',
+        'social_media_hours',
+        'netflix_hours',
+        'attendance_percentage',
+        'sleep_hours',
+        'exercise_frequency',
+        'mental_health_rating'
+    ]
+]
+
+# Selecting target
+y = df['exam_score']
+
+# Splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+# Creating and training the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Evaluate performance
+MAE = mean_absolute_error(y_test, y_pred)
+MSE = mean_squared_error(y_test, y_pred)
+
+# Displaying results
+print("Linear Regression Model - Performance Results")
+print(f"MAE: {MAE:.2f}")
+print(f"MSE: {MSE:.2f}")
